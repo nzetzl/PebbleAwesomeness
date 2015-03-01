2258eade4b9f7e279885b56bd8df991f9330a6a7
nzetzl <nzetzl@purdue.edu>
2015-03-01 11:01:52 -0600

    switched out with fixed program from cloudpebble

--- WOTD/src/WOTD.c
+++ WOTD/src/WOTD.c
@@ -4,17 +4,42 @@
 static Window *window;
 static TextLayer *heading_layer;
 static TextLayer *text_layer;
+static GFont word_font;
+static GFont title_font;
+static int x = 0;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  	text_layer_set_text(text_layer, "Select");
+	
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  	text_layer_set_text(text_layer, "Up");
+  	if (x < 0) {
+		x++;
+	}
+	if (x < -2) {
+		text_layer_set_text(text_layer, "Come back again tomorrow for more words!");
+	} else if (x == -2) {
+		text_layer_set_text(text_layer, "Butyraceous (adj.): of the nature of, resembling");
+	} else if (x == -1) {
+		text_layer_set_text(text_layer, 
+							"Ad Hockery (n.): reliance on temporary solutions rather than on consistent, long-term plans");
+	} else if (x == 0) {
+		text_layer_set_text(text_layer, "Emblem (n.): a sign, design, or figure that identifies or represents something.");
+	}
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
- 	 text_layer_set_text(text_layer, "Down");
+ 	 x--;
+	if (x < -2) {
+		text_layer_set_text(text_layer, "Come back again tomorrow for more words!");
+	} else if (x == -2) {
+		text_layer_set_text(text_layer, "Butyraceous (adj.): of the nature of, resembling");
+	} else if (x == -1) {
+		text_layer_set_text(text_layer, 
+							"Ad Hockery (n.): reliance on temporary solutions rather than on consistent, long-term plans");
+	} else if (x == 0) {
+		text_layer_set_text(text_layer, "Emblem (n.): a sign, design, or figure that identifies or represents something.");
+	}
 }
 
 static void click_config_provider(void *context) {
@@ -26,11 +51,15 @@ static void click_config_provider(void *context) {
 static void window_load(Window *window) {
   	Layer *window_layer = window_get_root_layer(window);
   	GRect bounds = layer_get_bounds(window_layer);
-	heading_layer = text_layer_create(GRect(5, 5, 50, 50));
-  	text_layer = text_layer_create(GRect(0, 100, 144, 25));
+	heading_layer = text_layer_create(GRect(20, 5, 100, 100));
+  	text_layer = text_layer_create(GRect(5, 55, 135, 80));
+	title_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+	word_font = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+	text_layer_set_font(heading_layer, title_font);
+	text_layer_set_font(text_layer, word_font);
   	text_layer_set_text(heading_layer, "Word of the Day");
-	text_layer_set_font(heading_layer, GOTHIC_24_BOLD);
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	text_layer_set_text(text_layer, "Emblem (n.): a sign, design, or figure that identifies or represents something.");
+	text_layer_set_text_alignment(text_layer, GTextAlignmentLeft);
   	text_layer_set_text_alignment(heading_layer, GTextAlignmentCenter);
   	layer_add_child(window_layer, text_layer_get_layer(heading_layer));
 	layer_add_child(window_layer, text_layer_get_layer(text_layer));
